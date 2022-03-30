538b9cee2d4ff1efc5a5033ff82b09d553b6deb2
Dennycbo <94752076+Dennycbo@users.noreply.github.com>
2022-03-30 13:33:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 let app = express()
 app.use(express.json())
+app.get("/", (req,res)=>{
+res.json({value:"Hello user"})
+})
 app.post("/webhook", (req, res)=>{
-    console.log(req.body)
+    res.JSON(req.body)
 })
 app.listen(3000, ()=>{
     console.log("Running.")
